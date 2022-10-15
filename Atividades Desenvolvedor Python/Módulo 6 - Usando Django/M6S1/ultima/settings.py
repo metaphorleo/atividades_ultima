876fcc4b20c47cf6e59ef35269cd959d82305ba8
metaphorleo <leo.trin.dade1999@gmail.com>
2022-10-15 17:34:00 -0300

    corrigindo leak de secret key django

--- Atividades Desenvolvedor Python/Módulo 6 - Usando Django/M6S1/ultima/settings.py
+++ Atividades Desenvolvedor Python/Módulo 6 - Usando Django/M6S1/ultima/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8b097#3+-r#@xd2)%51%y&johs40%%8gf1820ivp-^18^l00$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
