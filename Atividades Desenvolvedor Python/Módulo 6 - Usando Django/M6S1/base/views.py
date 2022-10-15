449a6347bc80498d1520922a3cc422d1cddfea52
metaphorleo <leo.trin.dade1999@gmail.com>
2022-10-15 01:42:03 -0300

    upando atividade do modulo 6 semana 1

---
+++ Atividades Desenvolvedor Python/Módulo 6 - Usando Django/M6S1/base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def contato(request):
+    return render(request, 'contato.html')
