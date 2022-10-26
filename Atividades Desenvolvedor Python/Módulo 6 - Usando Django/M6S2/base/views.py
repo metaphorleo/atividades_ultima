8ab5c48b2f276e18e36dae06e0501c5c35851920
Leonardo Trindade <leo.trin.dade1999@gmail.com>
2022-10-26 18:52:06 -0300

    deletando atividade

--- Atividades Desenvolvedor Python/Módulo 6 - Usando Django/M6S2/base/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def inicio(request):
-    return render(request, 'inicio.html')
-
-def contato(request):
-    return render(request, 'contato.html')
