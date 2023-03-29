286622a3ec5b765067f791203187f7c82a2cde2b
Leonardo <leo.trin.dade1999@gmail.com>
2023-03-29 00:16:30 -0300

    Incluindo novo endpoint para tamanhos

--- Módulo 7 - Django API/M7S4/rest_api/urls.py
+++ Módulo 7 - Django API/M7S4/rest_api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_api.views import AgendamentoModelViewSet, PetshopModelViewSet
 
@@ -8,6 +8,10 @@ router = DefaultRouter(trailing_slash=False)
 router.register('agendamento', AgendamentoModelViewSet)
 router.register('petshop', PetshopModelViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path('', include(router.urls)),
+    path('categoria/', AgendamentoModelViewSet.as_view({'get': 'categoria'}), name='categoria'),
+    path('categoria/<int:tamanho_id>/', AgendamentoModelViewSet.as_view({'get': 'categoria_por_id'}), name='categoria-por-id'),
+]
 
 urlpatterns += router.urls
