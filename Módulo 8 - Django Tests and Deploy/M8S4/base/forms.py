b21bfbff221b65a11b706a3e141a4b3bd7642dd7
Leonardo <leo.trin.dade1999@gmail.com>
2023-04-13 18:54:36 -0300

    CURSO CONCLUÍDO

--- Módulo 8 - Django Tests and Deploy/M8S4/base/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from base.models import Contato
-
-class ContatoForm(forms.ModelForm):
-    class Meta:
-        model = Contato
-        fields = ["nome", "email", "mensagem"]
